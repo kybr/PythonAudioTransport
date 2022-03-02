f73304a958194898ddcd5edcaef6dd51efbec605
Jack Kilgore <kilgorej99@gmail.com>
2022-03-01 16:56:23 -0800

    clean up code

--- audio_transport.py
+++ audio_transport.py
@@ -6,100 +6,91 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import pyaudio
 from tqdm import tqdm
-import time
+from time import time
 
 SAMPLERATE = 48000
 AUDIO_FILE1 = "440sine48k.wav"
-AUDIO_FILE2 = "01 Soft Channel 001.wav"
+AUDIO_FILE2 = "440saw48k.wav"
 
-# load audio
-audiox, srx = librosa.load(AUDIO_FILE1, sr=SAMPLERATE)
-audioy, sry = librosa.load(AUDIO_FILE2, sr=SAMPLERATE)
+# def timing(f):
+#     @wraps(f)
+#     def wrap(args, **kw):
+#         ts = time()
+#         result = f(args, **kw)
+#         te = time()
+#         print 'func:%r args:[%r, %r] took: %2.4f sec' % \
+#           (f.name, args, kw, te-ts)
+#         return result
+#     return wrap
 
+class AudioPlayer:
 
-# For audio samples of different lengths: pad by wrapping wrap
-if len(audiox) > len(audioy):
-	audioy = np.pad(audioy, (0, len(audiox) - len(audioy)), mode='wrap')
-elif len(audioy) > len(audiox):
-	audiox = np.pad(audiox, (0, len(audioy) - len(audiox)), mode='wrap')
+	def __init__(self, audio_array):
+		self.index = 0
+		self.arr = audio_array
 
-audio_length = audiox.shape[0]
+	def audio_callback(self, in_data, frame_count, time_info, status):
+		# TODO: Loop audio_output_array (can we edit audio_output_array asynchronously?)
+		out = self.arr[self.index:frame_count + self.index]
+		self.index = (self.index + frame_count) % (self.arr.shape[0] // 2)
+		return (out, pyaudio.paContinue)
 
-fft_size = 8192
-num_bins = fft_size // 2 + 1
+def compute_transport_matrix(X_nT, Y_nT):	
+	# Only use magnitude (for now).
+	X_nT = np.abs(X_nT)
+	Y_nT = np.abs(Y_nT)
 
+	# initialize transport algorithm
+	num_bins = X_nT.shape[0]
+	T = X_nT.shape[1]
 
-# get stft
-wx, tx, X = stft(audiox, fs=SAMPLERATE, window='hann', nperseg=1024, nfft=fft_size)
-wy, ty, Y = stft(audioy, fs=SAMPLERATE, window='hann', nperseg=1024, nfft=fft_size)
-
-
-# freq * freq_to_idx = idx into w array
-freq_to_idx = (num_bins - 1) / (wx[-1])
-
-# sanity check
-assert wx.shape[0] == wy.shape[0]
-assert [a == b for a, b in zip(wx, wy)] # same frequency bins
-assert tx.shape[0] == ty.shape[0]
-assert [a == b for a, b in zip(tx, ty)] # same time bins
-
-# some useful variables for dimensions
-n = num_bins
-T = tx.shape[0] # Max number of FFT steps
-
-# initialize transport algorithm
-
-#PI_nnT = np.zeros((n, n, T)) # policy we are solving for
-PI_T = [[] for _ in range(T)]
-normsx_T = np.zeros(T)
-normsy_T = np.zeros(T)
+	#PI_nnT = np.zeros((n, n, T)) # policy we are solving for
+	PI_T = [[] for _ in range(T)]
+	normsx_T = np.zeros(T)
+	normsy_T = np.zeros(T)
 
 
-# Audio Tranport Algo for each STFT Bin
-for t in range(T):
+	# Audio Tranport Algo for each STFT Bin
+	for t in range(T):
 
-	i, j = 0, 0
+		i, j = 0, 0
 
-	# get normalized magnitude vectors
-	normsx_T[t] = np.linalg.norm(X[:, t])
-	normsy_T[t] = np.linalg.norm(Y[:, t])
+		# get normalized magnitude vectors
+		normsx_T[t] = np.linalg.norm(X_nT[:, t])
+		normsy_T[t] = np.linalg.norm(Y_nT[:, t])
 
-	X_ = X[:, t] / normsx_T[t]
-	Y_ = Y[:, t] / normsy_T[t]
+		X_ = X_nT[:, t] / normsx_T[t]
+		Y_ = Y_nT[:, t] / normsy_T[t]
 
-	px, py = X_[i], Y_[j]
+		px, py = X_[i], Y_[j]
 
-	# Audio Transport Algorithm
-	while True:
-		if px < py:
-			# PI_nnT[i][j][t] = px
-			PI_T[t].append((i, j, px))
-			i += 1
+		# Audio Transport Algorithm
+		while True:
+			if px < py:
+				PI_T[t].append((i, j, px))
+				i += 1
 
-			if i >= num_bins:
-				break
+				if i >= num_bins:
+					break
 
-			py = py - px
-			px = X_[i]
-		else:
-			# PI_nnT[i][j][t] = py
-			PI_T[t].append((i, j, py))
-			j += 1
+				py = py - px
+				px = X_[i]
+			else:
+				PI_T[t].append((i, j, py))
+				j += 1
 
-			if j >= num_bins:
-				break
+				if j >= num_bins:
+					break
 
-			px = px - py
-			py = Y_[j]
+				px = px - py
+				py = Y_[j]
 
-
-pya = pyaudio.PyAudio()
-
-# TODO: Find a profile - find a look at which part is taking so long
-# TODO: Try this on dummy spectrums, and just 1 spectrums
+	return PI_T, normsx_T, normsy_T
 
 # depends on a few of the variables we just calculated
 def calculate_interpolation(k, PI_T, wx, normsx_T, normsy_T):
+	START_TIME = time()
+	T = len(PI_T)
 	Z_nT = np.zeros_like(X)
 	# TODO: also deal with normalization "scaling is interpolated linearly over the interpolation"
 		# this means that for a value k, and timestep t, 
@@ -119,24 +110,65 @@ def calculate_interpolation(k, PI_T, wx, normsx_T, normsy_T):
 		Z_nT[:, t] *= (1 - k) * normsx_T[t] + k * normsy_T[t]
 
 	t, output = istft(Z_nT, fs=SAMPLERATE, window='hann', nperseg=1024, nfft=fft_size)
+	END_TIME = time()
+	print("COMPUTE INTERPOLATION TOTAL TIME:", END_TIME - START_TIME)
 	return output
+
+
+
+# load audio
+audiox, srx = librosa.load(AUDIO_FILE1, sr=SAMPLERATE)
+audioy, sry = librosa.load(AUDIO_FILE2, sr=SAMPLERATE)
+
+
+# For audio samples of different lengths: pad by wrapping wrap
+if len(audiox) > len(audioy):
+	audioy = np.pad(audioy, (0, len(audiox) - len(audioy)), mode='wrap')
+elif len(audioy) > len(audiox):
+	audiox = np.pad(audiox, (0, len(audioy) - len(audiox)), mode='wrap')
+
+audio_length = audiox.shape[0]
+
+fft_size = 2048
+num_bins = fft_size // 2 + 1
+
+
+# get stft
+START_TIME = time()
+
+wx, tx, X = stft(audiox, fs=SAMPLERATE, window='hann', nperseg=1024, nfft=fft_size)
+wy, ty, Y = stft(audioy, fs=SAMPLERATE, window='hann', nperseg=1024, nfft=fft_size)
+# T = tx.shape[0] # Max number of FFT steps
+END_TIME = time()
+print("STFT TIME:", END_TIME - START_TIME)
+
+
+# freq * freq_to_idx = idx into w array
+freq_to_idx = (num_bins - 1) / (wx[-1])
+
+# sanity check
+assert wx.shape[0] == wy.shape[0]
+assert [a == b for a, b in zip(wx, wy)] # same frequency bins
+assert tx.shape[0] == ty.shape[0]
+assert [a == b for a, b in zip(tx, ty)] # same time bins
+	
+
+# TODO: Find a profile - find a look at which part is taking so long
+# TODO: Try this on dummy spectrums, and just 1 spectrums
+START_TIME = time()
+PI_T, normsx_T, normsy_T = compute_transport_matrix(X,Y)
+END_TIME = time()
+print("COMPUTE TRANSPORT MATRIX TIME:", END_TIME - START_TIME)
+
 
 k = float(input("enter interpolation value (0 to 1, -1 to exit): "))
 
 aoa = calculate_interpolation(k, PI_T, wx, normsx_T, normsy_T)
+
 audio_output_array = np.concatenate([aoa, aoa])
 
-class AudioPlayer:
 
-	def __init__(self, audio_array):
-		self.index = 0
-		self.arr = audio_array
-
-	def audio_callback(self, in_data, frame_count, time_info, status):
-		# TODO: Loop audio_output_array (can we edit audio_output_array asynchronously?)
-		out = self.arr[self.index:frame_count + self.index]
-		self.index = (self.index + frame_count) % (self.arr.shape[0] // 2)
-		return (out, pyaudio.paContinue)
+pya = pyaudio.PyAudio()
 
 player = AudioPlayer(audio_output_array)
 
